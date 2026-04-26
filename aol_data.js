a18b48c762639986d8831ec55152f26a84dbd27b
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-04-26 09:33:46 -0500

    Add files via upload

---
+++ aol_data.js
@@ -0,0 +1,534 @@
+const AOL_DATA = [
+  {
+    user: "10257524",
+    query: "nascar humor",
+    time: "2006-03-19 11:06:25",
+    rank: "3",
+    url: "http://hometown.aol.com"
+  },
+  {
+    user: "10257524",
+    query: "nascar humor",
+    time: "2006-03-19 11:06:25",
+    rank: "3",
+    url: "http://hometown.aol.com"
+  },
+  {
+    user: "10262525",
+    query: "homes for sale sevier county tn",
+    time: "2006-04-15 21:51:30",
+    rank: "8",
+    url: "http://www.relohomesearch.com"
+  },
+  {
+    user: "10262525",
+    query: "century 21 a realty group gatlinburg tn",
+    time: "2006-05-14 14:27:01",
+    rank: "1",
+    url: "http://www.homesandland.com"
+  },
+  {
+    user: "10262525",
+    query: "mls for sevier county tn",
+    time: "2006-05-14 16:07:50",
+    rank: "2",
+    url: "http://www.homes.com"
+  },
+  {
+    user: "10262525",
+    query: "mls for sevier county tn",
+    time: "2006-05-14 16:07:50",
+    rank: "1",
+    url: "http://www.relohomesearch.com"
+  },
+  {
+    user: "10262525",
+    query: "mls for sevier county tn",
+    time: "2006-05-14 16:07:50",
+    rank: "4",
+    url: "http://www.homegain.com"
+  },
+  {
+    user: "10263554",
+    query: "washington mutual",
+    time: "2006-04-04 10:12:23",
+    rank: "2",
+    url: "http://www.wamuhomeloans.com"
+  },
+  {
+    user: "10263554",
+    query: "home depot",
+    time: "2006-05-22 11:48:30",
+    rank: "1",
+    url: "http://www.homedepot.com"
+  },
+
+  {
+    user: "10257759",
+    query: "broadway wicked",
+    time: "2006-03-29 18:13:14",
+    rank: "4",
+    url: "http://www.musicalschwartz.com"
+  },
+  {
+    user: "10257759",
+    query: "free sheet music",
+    time: "2006-04-26 01:17:22",
+    rank: "1",
+    url: "http://www.freesheetmusic.net"
+  },
+  {
+    user: "10257759",
+    query: "free sheet music",
+    time: "2006-04-26 01:17:22",
+    rank: "8",
+    url: "http://www.pianoworld.com"
+  },
+  {
+    user: "10257759",
+    query: "free sheet music",
+    time: "2006-04-26 01:17:22",
+    rank: "3",
+    url: "http://www.musicaviva.com"
+  },
+  {
+    user: "10257759",
+    query: "free download sheet music",
+    time: "2006-04-26 02:34:39",
+    rank: "1",
+    url: "http://www.freesheetmusic.net"
+  },
+  {
+    user: "10257759",
+    query: "free download sheet music",
+    time: "2006-04-26 02:36:33",
+    rank: "11",
+    url: "http://www.pitt.edu"
+  },
+  {
+    user: "10257759",
+    query: "free download sheet music",
+    time: "2006-04-26 02:36:33",
+    rank: "22",
+    url: "http://www.stagepass.com"
+  },
+  {
+    user: "10257759",
+    query: "free sheet music",
+    time: "2006-04-27 01:11:21",
+    rank: "1",
+    url: "http://www.freesheetmusic.net"
+  },
+  {
+    user: "10257759",
+    query: "current free sheet music",
+    time: "2006-04-27 01:14:07",
+    rank: "4",
+    url: "http://www.pianoworld.com"
+  },
+  {
+    user: "10257759",
+    query: "current free sheet music",
+    time: "2006-04-27 01:14:07",
+    rank: "7",
+    url: "http://www.pianofiles.com"
+  },
+  {
+    user: "10257759",
+    query: "current free sheet music",
+    time: "2006-04-27 01:14:07",
+    rank: "7",
+    url: "http://www.pianofiles.com"
+  },
+  {
+    user: "10266839",
+    query: "cascada",
+    time: "2006-03-11 19:12:08",
+    rank: "1",
+    url: "http://www.cascada-music.de"
+  },
+
+  {
+    user: "10262525",
+    query: "weather for sevierville tn",
+    time: "2006-03-21 07:25:04",
+    rank: "1",
+    url: "http://www.wunderground.com"
+  },
+  {
+    user: "10268200",
+    query: "weatherford college",
+    time: "2006-03-17 01:17:22",
+    rank: "1",
+    url: "http://www.wc.edu"
+  },
+
+  {
+    user: "10288049",
+    query: "happy when someone died",
+    time: "2006-04-04 20:39:08",
+    rank: "6",
+    url: "http://windowmanager.blogspot.com"
+  },
+  {
+    user: "10299486",
+    query: "sexual chocolate drink recipe",
+    time: "2006-05-27 17:32:44",
+    rank: "5",
+    url: "http://www.happy-hour.net"
+  },
+  {
+    user: "10358902",
+    query: "unhappy marriages",
+    time: "2006-05-15 23:02:19",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "10504459",
+    query: "www.happynews.com http",
+    time: "2006-03-20 11:50:02",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "10504459",
+    query: "www.happynews.com http",
+    time: "2006-03-20 11:51:37",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "10693493",
+    query: "reflexes",
+    time: "2006-03-22 11:11:35",
+    rank: "1",
+    url: "http://www.happyhub.com"
+  },
+  {
+    user: "10714991",
+    query: "computer games",
+    time: "2006-03-24 23:58:41",
+    rank: "5",
+    url: "http://www.happypuppy.com"
+  },
+  {
+    user: "10969993",
+    query: "nascar driver hometowns",
+    time: "2006-03-25 19:23:41",
+    rank: "2",
+    url: "http://happyhomeschool.familyclassroom.net"
+  },
+  {
+    user: "11021518",
+    query: "happyeasterfrompt",
+    time: "2006-04-16 09:27:24",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "11025331",
+    query: "english to arabic translation free",
+    time: "2006-04-09 20:50:36",
+    rank: "5",
+    url: "http://www.happychild.org.uk"
+  },
+  {
+    user: "11101110",
+    query: "happy puppy",
+    time: "2006-03-28 14:51:52",
+    rank: "1",
+    url: "http://www.happypuppy.com"
+  },
+  {
+    user: "11101110",
+    query: "happy puppy",
+    time: "2006-04-29 21:51:11",
+    rank: "1",
+    url: "http://www.happypuppy.com"
+  },
+
+  {
+    user: "10268200",
+    query: "www.321sexchat.com",
+    time: "2006-03-07 23:30:16",
+    rank: "1",
+    url: "http://www.adultlovecompass.com"
+  },
+  {
+    user: "10269261",
+    query: "online dating",
+    time: "2006-03-09 20:31:58",
+    rank: "3",
+    url: "http://www.lovecompass.com"
+  },
+  {
+    user: "10277730",
+    query: "guitar tabs i love this bar",
+    time: "2006-03-16 09:36:00",
+    rank: "2",
+    url: "http://www.countrytabs.com"
+  },
+  {
+    user: "10277730",
+    query: "guitar tabs i love this bar",
+    time: "2006-03-16 09:36:00",
+    rank: "3",
+    url: "http://www.tabcountry.com"
+  },
+  {
+    user: "10280363",
+    query: "corpus christi poker",
+    time: "2006-03-24 15:15:03",
+    rank: "3",
+    url: "http://loveandcasinowar.com"
+  },
+  {
+    user: "10285345",
+    query: "recipe for love",
+    time: "2006-05-01 19:39:48",
+    rank: "9",
+    url: "http://www.longlostperfume.com"
+  },
+  {
+    user: "10285345",
+    query: "receipes for love free",
+    time: "2006-05-01 19:49:51",
+    rank: "7",
+    url: "http://recipecircus.com"
+  },
+
+  {
+    user: "10266172",
+    query: "ear problems in dogs",
+    time: "2006-05-23 21:46:31",
+    rank: "2",
+    url: "http://animalpetdoctor.homestead.com"
+  },
+  {
+    user: "10267346",
+    query: "how to train a dog",
+    time: "2006-05-02 19:18:50",
+    rank: "1",
+    url: "http://www.perfectpaws.com"
+  },
+  {
+    user: "10267346",
+    query: "adopting a dog",
+    time: "2006-05-02 19:31:25",
+    rank: "1",
+    url: "http://www.petfinder.com"
+  },
+  {
+    user: "10267346",
+    query: "dog pound",
+    time: "2006-05-02 19:46:36",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "10268200",
+    query: "burt ward dog rescue",
+    time: "2006-03-30 01:37:42",
+    rank: "1",
+    url: "http://www.gentlegiantsrescue.com"
+  },
+  {
+    user: "10269192",
+    query: "frontline for dogs",
+    time: "2006-05-22 21:42:33",
+    rank: "",
+    url: ""
+  },
+  {
+    user: "10270375",
+    query: "mexico world classic baseball cap",
+    time: "2006-04-12 12:14:38",
+    rank: "2",
+    url: "http://www.fogdog.com"
+  },
+  {
+    user: "10270375",
+    query: "mexico world classic baseball cap",
+    time: "2006-04-12 12:14:38",
+    rank: "2",
+    url: "http://www.fogdog.com"
+  },
+  {
+    user: "10274152",
+    query: "chinese cresteds",
+    time: "2006-04-17 06:06:42",
+    rank: "1",
+    url: "http://www.dogbreedinfo.com"
+  },
+  {
+    user: "10274152",
+    query: "dog gum disease",
+    time: "2006-05-18 19:39:44",
+    rank: "2",
+    url: "http://www.peteducation.com"
+  },
+  {
+    user: "10277730",
+    query: "motorola v551 manual",
+    time: "2006-04-14 12:24:03",
+    rank: "8",
+    url: "http://www.phonedog.com"
+  },
+
+  {
+    user: "10271295",
+    query: "university of texas",
+    time: "2006-03-06 13:40:20",
+    rank: "1",
+    url: "http://www.utexas.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of texas",
+    time: "2006-03-06 13:40:20",
+    rank: "2",
+    url: "http://www.uta.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of texas",
+    time: "2006-03-06 13:40:20",
+    rank: "3",
+    url: "http://www.utsystem.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of texas",
+    time: "2006-03-06 13:40:20",
+    rank: "3",
+    url: "http://www.utsystem.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of houston tx",
+    time: "2006-03-06 13:52:25",
+    rank: "3",
+    url: "http://www.uhd.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of houston tx",
+    time: "2006-03-06 13:52:25",
+    rank: "1",
+    url: "http://www.uh.edu"
+  },
+  {
+    user: "10271295",
+    query: "university of houston tx",
+    time: "2006-03-06 13:52:25",
+    rank: "2",
+    url: "http://www.rice.edu"
+  },
+  {
+    user: "10271295",
+    query: "houston university",
+    time: "2006-03-06 14:08:55",
+    rank: "1",
+    url: "http://www.uh.edu"
+  },
+  {
+    user: "10271295",
+    query: "houston university",
+    time: "2006-03-06 14:08:55",
+    rank: "2",
+    url: "http://www.shsu.edu"
+  },
+  {
+    user: "10271295",
+    query: "houston university",
+    time: "2006-03-06 14:08:55",
+    rank: "5",
+    url: "http://uhcougars.collegesports.com"
+  },
+
+  {
+    user: "10488224",
+    query: "high protein diets and alt results",
+    time: "2006-03-07 18:44:52",
+    rank: "1",
+    url: "http://www.amarillomed.com"
+  },
+  {
+    user: "10488224",
+    query: "high protein diets and alt results",
+    time: "2006-03-07 18:44:52",
+    rank: "8",
+    url: "http://www.medhelp.org"
+  },
+  {
+    user: "10488224",
+    query: "high protein diets and alt results",
+    time: "2006-03-07 18:44:52",
+    rank: "1",
+    url: "http://www.amarillomed.com"
+  },
+  {
+    user: "10488224",
+    query: "high protein diet health risks",
+    time: "2006-03-07 18:58:01",
+    rank: "6",
+    url: "http://news.bbc.co.uk"
+  },
+  {
+    user: "10488224",
+    query: "high protein diet health risks",
+    time: "2006-03-07 18:58:01",
+    rank: "3",
+    url: "http://www.annecollins.com"
+  },
+  {
+    user: "10488224",
+    query: "blood test results and high protein diets",
+    time: "2006-03-07 19:01:23",
+    rank: "2",
+    url: "http://www.bodybuildingforyou.com"
+  },
+  {
+    user: "10488224",
+    query: "blood test results and high protein diets",
+    time: "2006-03-07 19:01:23",
+    rank: "1",
+    url: "http://www.amarillomed.com"
+  },
+  {
+    user: "10488224",
+    query: "blood test results and high protein diets",
+    time: "2006-03-07 19:01:23",
+    rank: "9",
+    url: "http://www.biblelife.org"
+  },
+  {
+    user: "10488224",
+    query: "alt levels and high protein diets",
+    time: "2006-03-07 19:15:04",
+    rank: "8",
+    url: "http://www.medhelp.org"
+  },
+  {
+    user: "10488224",
+    query: "alt levels and high protein diets",
+    time: "2006-03-07 19:15:04",
+    rank: "9",
+    url: "http://www.fasebj.org"
+  },
+  {
+    user: "10488224",
+    query: "alt levels and high protein diets",
+    time: "2006-03-07 19:15:04",
+    rank: "8",
+    url: "http://www.medhelp.org"
+  },
+  {
+    user: "10626568",
+    query: "high protein in urine",
+    time: "2006-03-25 14:59:31",
+    rank: "1",
+    url: "http://www.netdoctor.co.uk"
+  }
+];
